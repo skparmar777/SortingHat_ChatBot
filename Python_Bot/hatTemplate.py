dcf7a526187de55c951fb645e9831378de23d9ba
Sejal K. Parmar <skparmar701@gmail.com>
2019-04-06 00:58:07 -0500

    adding template

---
+++ Python_Bot/hatTemplate.py
@@ -0,0 +1,16 @@
+class hat:
+    def __init__(self, *args, **kwargs):
+        # put basic startup logic here
+        self.sort()
+        self.sorting_results()
+
+
+    def sort(self):
+        # put sorting logic here
+        # compute results of sorting
+        return None
+
+    def sorting_results(self):
+        # put logic tat figures out houses here
+        # print out results
+        return None
